4343017c6a8e68e8ad574f9e63b9825afb96bf43
Fernando <fernandobarcalarodriguez@gmail.com>
2020-12-07 22:11:33 +0100

    added window explode effect

---
+++ JailEscape/Assets/Script/Window.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Window : MonoBehaviour
+{
+    public ParticleSystem OnDestroyEffect;
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "JailMan")
+            Destroy(gameObject);
+    }
+    //public void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if(collision.gameObject.name == "JailMan")
+    //        Destroy(gameObject);
+    //}
+
+    public void OnDestroy()
+    {
+        var quaternion = Quaternion.identity;
+        quaternion.x = 197.43f;
+        quaternion.y = -90f;
+        quaternion.z = 90f;
+        var pos = transform.position;
+        Instantiate(OnDestroyEffect, pos, quaternion);
+    }
+}
